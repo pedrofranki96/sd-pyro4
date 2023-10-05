9fa26e0352813147381bb7b858a25f1ced9929ad
Pedro Frankiewicz <pedro.frankiewicz@gmail.com>
2023-10-05 15:27:12 -0300

    warehouse example

---
+++ visit.py
@@ -0,0 +1,13 @@
+import sys
+import Pyro4
+from person import Person
+
+if sys.version_info<(3,0):
+    input = raw_input
+
+uri = input("Enter the uri of the warehouse: ").strip()
+warehouse = Pyro4.Proxy(uri)
+janet = Person("Janet")
+henry = Person("Henry")
+janet.visit(warehouse)
+henry.visit(warehouse)
